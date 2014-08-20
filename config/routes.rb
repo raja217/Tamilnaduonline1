a0f9fd1edccce6687302f4c851c3ba85ff57146c
Eedhal <eedhalsofts@gmail.com>
2014-08-20 12:18:02 +0530

    Added Codeship image

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ CMS::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'main#index'
+   root :to => 'user#login'
 
   # See how all your routes lay out with "rake routes"
 
